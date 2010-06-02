0a53be6065bf8775bf31993c8ecbfdb83fd809e6
svn <svn@95ad4591-2629-e011-b8a3-0018f302bd40>
2010-06-02 09:12:33 +0000

    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@31 95ad4591-2629-e011-b8a3-0018f302bd40

--- libs/smarty/plugins/function.htmlview.php
+++ libs/smarty/plugins/function.htmlview.php
@@ -1,25 +1,26 @@
 <?php
 function smarty_function_htmlview($params, &$smarty)
 {
-	$ret='';
-	$n=$params['item'];
-	$tpl=gs_tpl::get_instance();
-	$field=$params['field'];
+		    $n=$params['item'];
+		    $tpl=gs_tpl::get_instance();
+		    $field=$params['field'];
+		    
+		    $field_name=isset($params['name_format']) ? sprintf($params['name_format'],$field) : $field;
+		    
+		    $structure=$n->get_recordset()->structure['htmlforms'][$field];
+		    $options=$n->get_recordset()->structure['fields'][$field];
 
-	$field_name=isset($params['name_format']) ? sprintf($params['name_format'],$field) : $field;
-
-	$value=isset($data[$field]) ? $data[$field] : (isset($params['value']) ? $params['value'] : $n[$field] );
-
-	$structure=$n->get_recordset()->structure['htmlforms'][$field];
-	$options=$n->get_recordset()->structure['fields'][$field];
-	switch ($structure['type']) {
-		case 'image':
-			$ret=$n->$field->first() ? sprintf("<img src='/img/%s/small.jpg'>",$n->$field->first()->get_id()) : '';
-			break;
-		default:
-			$ret=$n->$field;
-			break;
-	}
-	return $ret;
+			
+		    switch ($structure['type']) {
+			    case 'image':
+			    	$ret=sprintf("<img src='/img/%d/small.jpg'>",$n->$field_name->first()->get_id());
+				break;
+			    case 'checkbox':
+			    	$ret=sprintf("<input type='checkbox' readonly='readonly' disabled='disabled' %s>",$n->$field_name?'checked':'');
+				break;
+			     default:
+			     	$ret=$n->$field_name;
+		    }
+		    return $ret;
 }
 ?>
