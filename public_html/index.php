39fd76e56ac914f8adcfcc8d9916ea97d9653de9
svn <svn@95ad4591-2629-e011-b8a3-0018f302bd40>
2011-01-26 08:41:29 +0000

    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@365 95ad4591-2629-e011-b8a3-0018f302bd40

---
+++ public_html/index.php
@@ -0,0 +1,18 @@
+<?php
+ob_start();
+require_once(dirname(__FILE__).'/libs/config.lib.php');
+$gs_node_id=1;
+$cfg=gs_config::get_instance();
+if (strpos($_SERVER['REQUEST_URI'],'/superadmin')===0) $init=new gs_init('superadmin');
+	else $init=new gs_init('auto');
+
+$init->init(LOAD_CORE | LOAD_STORAGE | LOAD_TEMPLATES);
+$init->load_modules();
+
+session_start();
+
+$o_h=new gs_parser($init->data);
+$o_h->process();
+
+
+?>
---
+++ public_html/install.php
@@ -0,0 +1,14 @@
+<?php
+require_once(dirname(__FILE__).'/../libs/config.lib.php');
+$gs_node_id=1;
+$init=new gs_init('user');
+$init->init(LOAD_CORE | LOAD_STORAGE | LOAD_TEMPLATES);
+
+
+$init->load_modules();
+$init->install_modules();
+gs_fkey::update_fkeys();
+
+gs_logger::dump();
+
+?>
