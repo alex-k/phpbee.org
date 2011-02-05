9f5da7d6def6c827183f6cf31f7ffc3b51262571
svn <svn@95ad4591-2629-e011-b8a3-0018f302bd40>
2011-02-05 16:56:03 +0000

    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@105 95ad4591-2629-e011-b8a3-0018f302bd40

--- libs/smarty/plugins/function.handler.php
+++ libs/smarty/plugins/function.handler.php
@@ -1,23 +1,17 @@
 <?php
 function smarty_function_handler($params, &$smarty) {
-	/*
-	var_dump($smarty->get_template_vars('_gsdata'));
-	var_dump($smarty->get_template_vars('_gsparams'));
-	var_dump($params);
-	*/
-
 	$data=$smarty->get_template_vars('_gsdata');
-	/*
-	$subdir=$smarty->get_template_vars('_module_subdir');
-	if($subdir) $params['gspgid']=$subdir.'/'.$params['gspgid'];
-	*/
+	$gsparams=$smarty->get_template_vars('_gsparams');
+
 	$params['gspgid']=trim($params['gspgid'],'/');
 	if (!isset($data['gspgid_root'])) {
 		$data['gspgid_root']=$data['gspgid'];
 	}
 	$data['gspgid_handler']=$data['gspgid'];
 	$data['gspgid']=$params['gspgid'];
-	$data=array_merge($data,$params);
+	$data['handler_params']=$params;
+	//$data=array_merge($data,$params);
+
 	$smarty->assign('gspgid_form',$data['gspgid']);
 	$smarty->assign('gspgid_handler',$data['gspgid_handler']);
 	$smarty->assign('gspgid_root',$data['gspgid_root']);
