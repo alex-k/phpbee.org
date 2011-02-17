18294aee62923d52bf00232ed858befa06c64c63
svn <svn@95ad4591-2629-e011-b8a3-0018f302bd40>
2011-02-17 09:22:21 +0000

    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@133 95ad4591-2629-e011-b8a3-0018f302bd40

--- libs/smarty/plugins/function.handler_block.php
+++ libs/smarty/plugins/function.handler_block.php
@@ -1,5 +1,5 @@
 <?php
-function smarty_function_block($params, &$smarty)
+function smarty_function_handler_block($params, &$smarty)
 {
                     //$smarty->trigger_error("html_image: extra attribute '$_key' cannot be an array", E_USER_NOTICE);
 		    $blocks=$smarty->get_template_vars('_blocks');
