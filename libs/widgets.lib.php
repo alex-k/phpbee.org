d338373a69931c76dbe334250ad8dabc8d9fdfed
svn <svn@95ad4591-2629-e011-b8a3-0018f302bd40>
2011-04-29 13:18:52 +0000

    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@159 95ad4591-2629-e011-b8a3-0018f302bd40

--- libs/widgets.lib.php
+++ libs/widgets.lib.php
@@ -349,7 +349,7 @@ class gs_widget_alex_gal extends gs_widget {
 				
 
 
-				$s.=sprintf('<a href="/admin/alex_gal/%s/%s/%d/%s" target="gal_%s" id="lMany2One_%s">%s</a>',
+				$s.=sprintf('<a href="alex_gal/%s/%s/%d/%s" target="gal_%s">%s</a>',
 						$this->params['options']['recordset'],
 						$this->params['options']['foreign_field_name'],
 						$rid,
