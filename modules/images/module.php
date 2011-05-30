8856f24622fd1aab0add186aa3dc1d970c958cb9
svn <svn@95ad4591-2629-e011-b8a3-0018f302bd40>
2011-05-30 11:59:14 +0000

    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@403 95ad4591-2629-e011-b8a3-0018f302bd40

--- modules/images/module.php
+++ modules/images/module.php
@@ -1,42 +1,9 @@
 <?php
-gs_dict::append(array(
-		'LOAD_IMAGES'=>'добавить картинки',
-	));
-
-
-class module_images  extends gs_base_module implements gs_module {
-	function __construct() { }
-	function install() {
-		foreach(array() as $r){
-			$this->$r=new $r;
-			$this->$r->install();
-		}
-	}
-	function get_menu() {
-		return '<a href="/admin/file_images/">Images</a>';
-	}
-	
-	static function get_handlers() {
-		$data=array(
-		'default'=>array(
-			'default'=>'gs_base_handler.show404:{name:404.html}',
-		),
-		'get_post'=>array(
-			''=>'gs_base_handler.show:{name:images.html}',
-			'/admin/file_images'=>'gs_base_handler.show:{name:adm_images.html:classname:tw_news_images}',
-			//'/admin/form/tw_news_images'=>'gs_base_handler.post:{name:form.html:classname:tw_news_images:form_class:form_admin}',
-			'/admin/file_images/delete'=>'admin_handler.deleteform:{classname:tw_news_images}',
-		),
-	);
-	return self::add_subdir($data,dirname(__file__));
-	}
-}
-
 class tw_file_images extends gs_recordset_short{
 	var $gs_connector_id='file_public';
 	var $fields=array(
-		'File'=> "fFile 'Файлик'",
-		'Name'=> "fString 'Названице' ",
+		'File'=> "fFile 'Файл'",
+		//'Name'=> "fString 'Названице' ",
 		//'desc'=> "fText",
 	);
 	function __construct($init_opts=false) {
