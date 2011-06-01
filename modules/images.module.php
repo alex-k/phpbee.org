be3e1c8be89e3fa6f8b0386411480e98ae4dc285
svn <svn@95ad4591-2629-e011-b8a3-0018f302bd40>
2011-06-01 12:44:14 +0000

    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@406 95ad4591-2629-e011-b8a3-0018f302bd40

--- modules/images.module.php
+++ modules/images.module.php
@@ -19,6 +19,7 @@ abstract class tw_images extends gs_recordset_short {
 }
 abstract class tw_file_images extends gs_recordset_short{
 	var $gs_connector_id='file_public';
+	var $config=array();
 	var $fields=array(
 		'File'=> "fFile 'Файл'",
 		//'Name'=> "fString 'Названице' ",
@@ -26,6 +27,9 @@ abstract class tw_file_images extends gs_recordset_short{
 	);
 	function __construct($init_opts=false) {
 		parent::__construct($this->fields,$init_opts);
+		$this->config_previews();
+		$this->structure['triggers']['after_insert']='resize';
+		$this->structure['triggers']['after_update']='resize';
 	}
 	/*
         function record_as_string($rec) {
@@ -49,11 +53,35 @@ abstract class tw_file_images extends gs_recordset_short{
 	function src($params,$record=null) {
 		$records=$record ? array($record) : $this;
 		$ret=array();
-		$fname=$this->get_connector()->www_root.DIRECTORY_SEPARATOR.$this->db_tablename;
+		$fname=$this->get_connector()->www_root.'/'.$this->db_tablename;
 		foreach ($records as $rec) {
-			$ret[]=$fname.DIRECTORY_SEPARATOR.$this->get_connector()->split_id($rec->get_id()).DIRECTORY_SEPARATOR.'File_data';	
+			$ret[]=$fname.'/'.$this->get_connector()->split_id($rec->get_id()).'/'.'File_data';	
 		}
 		return $ret;
+	}
+	
+	function config_previews() {
+		$this->config=array(
+			'orig'=>array('width'=>0,'height'=>0,'method'=>'copy'),
+		);
+	}
+	
+	function resize($rec,$type) {
+		$fname=$this->get_connector()->root.DIRECTORY_SEPARATOR.$this->db_tablename.DIRECTORY_SEPARATOR.$this->get_connector()->split_id($rec->get_id()).DIRECTORY_SEPARATOR;
+		$gd=new vpa_gd($rec->first()->File_data,false);
+		foreach ($this->config as $key => $data) {
+			$sname=$fname.'File_data';
+			$iname=$fname.$key.'.jpg';
+			if ($data['width']>0  && ($data['width']<$rec->first()->File_width || $data['heigt']<$rec->first()->File_height)) {
+				if ($data['bgcolor']) $gd->set_bg_color($data['bgcolor'][0],$data['bgcolor'][0],$data['bgcolor'][0]);
+				$gd->resize($data['width'],$data['height'],$data['method']);
+			}
+			if (isset($data['method']) && $data['method']=='copy') {
+				copy($sname,$iname);
+			} else {
+				$gd->save($iname,100);
+			}
+		}
 	}
 
 }
