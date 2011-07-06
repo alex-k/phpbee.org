0597407fd2cf7113458524ec8a41c4b0fe9493f1
svn <svn@95ad4591-2629-e011-b8a3-0018f302bd40>
2011-07-06 15:41:40 +0000

    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@434 95ad4591-2629-e011-b8a3-0018f302bd40

--- public_html/index.php
+++ public_html/index.php
@@ -3,6 +3,7 @@ ob_start();
 require_once(dirname(__FILE__).'/../libs/config.lib.php');
 $gs_node_id=1;
 $cfg=gs_config::get_instance();
+mlog('1');
 
 if (strpos($_SERVER['REQUEST_URI'],'/superadmin')===0) $init=new gs_init('superadmin');
 	else $init=new gs_init('auto');
@@ -10,13 +11,12 @@ cfg_set('tpl_data_dir',array(
 	cfg('tpl_data_dir'),
 	realpath(cfg('root_dir').'html'),
 ));
-$init->init(LOAD_CORE | LOAD_STORAGE | LOAD_TEMPLATES);
-$init->load_modules();
-mlog('1');
+//$init->init(LOAD_CORE | LOAD_STORAGE | LOAD_TEMPLATES | LOAD_EXTRAS);
+$init->init(LOAD_CORE);
+//$init->load_modules();
 session_start();
 
 $o_h=new gs_parser($init->data);
 $o_h->process();
-
 
 ?>
--- public_html/install.php
+++ public_html/install.php
@@ -2,12 +2,13 @@
 require_once(dirname(__FILE__).'/../libs/config.lib.php');
 $gs_node_id=1;
 $init=new gs_init('user');
-$init->init(LOAD_CORE | LOAD_STORAGE | LOAD_TEMPLATES);
+$init->init(LOAD_CORE | LOAD_STORAGE | LOAD_TEMPLATES | LOAD_EXTRAS);
 
 
 $init->compile_modules();
 $init->load_modules();
 $init->install_modules();
+$init->save_handlers();
 gs_fkey::update_fkeys();
 
 gs_logger::dump();
