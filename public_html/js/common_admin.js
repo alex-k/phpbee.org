558830f32bbde1314e40405bc72724b2933735f5
svn <svn@95ad4591-2629-e011-b8a3-0018f302bd40>
2011-10-13 13:44:28 +0000

    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@481 95ad4591-2629-e011-b8a3-0018f302bd40

--- public_html/js/common_admin.js
+++ public_html/js/common_admin.js
@@ -1,6 +1,7 @@
 $(document).ready (function (){
 	$(".lMany2Many").gs_multiselect();
 	$(".fDateTime").datepicker();
+	$(".fDateTimeFilter").datepicker();
 	$('.fWysiwyg').rte({
 		//css: ['default.css'],
 		controls_rte: rte_toolbar,
