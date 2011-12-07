a6d53a1e7c7db5622ce415435f9350ee12ba9b7c
svn <svn@95ad4591-2629-e011-b8a3-0018f302bd40>
2011-12-07 14:10:45 +0000

    form.interaction
    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@543 95ad4591-2629-e011-b8a3-0018f302bd40

---
+++ public_html/js/jquery.form.interaction.js
@@ -0,0 +1,77 @@
+(function($) {
+
+	var options = {};
+
+	var events = {
+		'.fRadio':'change',	
+		'.fCheckbox':'change',	
+		'.fSelect':'change',	
+		};
+	
+	$.fn.interaction= function(params) {
+		options = $.extend({}, $.fn.interaction.defaults, params);
+
+		show = function(obj) {
+			obj.show();
+		}
+		hide= function(obj) {
+			obj.hide();
+		}
+				
+		answer = function(data) {
+			for ( k in data) {
+				var d=data[k];
+				var obj=$('[name='+d.field+']');
+				var i_box=obj.closest('.interact_box');
+				if (i_box.size()) obj=i_box;
+				self[d.action](obj);
+			}
+		}
+		postform=function() {
+			var form = $(this).closest('form');
+			var data = {} ;
+			data['gsform_interact'] = this.name;
+			data['gspgid_form']=$('[name=gspgid_form]',form).val();
+			$('.fInteract',form).each(function() {
+				if ($(this).hasClass('fCheckbox')) {
+					data[this.name]=this.checked ? 1 : 0;
+				} else if ($(this).hasClass('fRadio')) {
+					if (this.checked) data[this.name]=this.value;
+				} else {
+					data[this.name]=this.value;
+				}
+			});
+
+			$.ajax({
+				url:document.location.href,
+				data: data,
+				type: 'POST',
+				dataType: 'JSON',
+				success : answer
+			});
+
+
+		}
+
+		return this.each(function() {
+
+			$('.fInteract',this).each(function() {
+				for (ev in events) {
+					$(this).filter(ev).bind(events[ev],postform);
+				}
+
+			});
+
+		});
+
+	};
+
+
+	
+	
+	
+	$.fn.interaction.defaults = {
+		
+	};
+	
+})(jQuery);
