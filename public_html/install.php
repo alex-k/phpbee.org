7829def1e4280db6859654d732af0ac3ff99a5ef
svn <svn@95ad4591-2629-e011-b8a3-0018f302bd40>
2011-12-15 06:27:12 +0000

    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@557 95ad4591-2629-e011-b8a3-0018f302bd40

--- public_html/install.php
+++ public_html/install.php
@@ -6,6 +6,8 @@ if (class_exists('Phar',0) && file_exists(dirname(__FILE__).'/../gs_libs.phar.gz
 }
 $gs_node_id=1;
 $init=new gs_init('user');
+$cfg=gs_config::get_instance();
+$cfg->check_install_key();
 $init->init(LOAD_CORE | LOAD_STORAGE | LOAD_TEMPLATES | LOAD_EXTRAS);
 
 $init->clear_cache();
