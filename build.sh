df6f177fe2d9775d5baba6aee5e4aa71f715ffd9
alexk <alexk@95ad4591-2629-e011-b8a3-0018f302bd40>
2012-01-23 12:03:45 +0000

    new repository
    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@593 95ad4591-2629-e011-b8a3-0018f302bd40

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
+REPO="file:///home/pahomov/www/phpbee.org/svn/phpbee.org"
+LASTFNAME="public_html/download/`ls -tr public_html/download/ | tail -n1`";
+MTIME=`stat -n -f '%c' $LASTFNAME`
+LASTDATE=`date -r $MTIME "+%Y-%m-%d"  `
+
+FNAME="phpbee-`date "+%d%b%y"`.zip"
+echo $FNAME
+
 
 mkdir build
 cd build
-svn export svn+ssh://svn@ytech.ru/home/svn/gs_blank_site . --force
+svn log -r '{'$LASTDATE'}':'{'`date -v +1d "+%Y-%m-%d"`'}' --xml --verbose $REPO > Changelog.xml
+xsltproc ../svn2cl.xsl Changelog.xml  > Changelog.txt
+xsltproc ../svn2html.xsl Changelog.xml  > Changelog.html
+svn export $REPO . --force
 mv default.config.php config.php
 chmod 777 config.php var
 mv html/index_page_default.html html/index.html
@@ -10,10 +21,13 @@ php phar.php
 find . -name public_html -mindepth 2 -exec sh -c "L=\`dirname {}\`; mkdir -p public_html/\$L; cp -r {}/* public_html/\$L ; " \;
 rm -fr libs
 cp public_html/worker.php public_html/index.php
-zip -r phpbee.zip config.php gs_libs.phar.gz html modules packages public_html 
+zip -r phpbee.zip config.php gs_libs.phar.gz html modules packages public_html Changelog.txt
 zip phpbee.zip var
 mv phpbee.zip ..
 cd ..
+cp build/Changelog.html public_html/download/Changelog-$FNAME.html
 rm -fr build
 
-cp phpbee.zip public_html/download/phpbee-`date "+%d%b%y"`.zip
+
+cp phpbee.zip public_html/download/$FNAME
+echo $FNAME > html/last_build.html
