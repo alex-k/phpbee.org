9470a959a04dd7e8ff6f92b0aabee22949123a3a
alexk <alexk@95ad4591-2629-e011-b8a3-0018f302bd40>
2012-02-08 13:19:43 +0000

    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@626 95ad4591-2629-e011-b8a3-0018f302bd40

--- build_copy.sh
+++ build_copy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mv default.config.php config.php
-chmod 777 config.php var
+chmod 777 config.php var modules
 mv html/index_page_default.html html/index.html
 mv html/404_default.html html/404.html
 cp public_html/worker.php public_html/index.php
