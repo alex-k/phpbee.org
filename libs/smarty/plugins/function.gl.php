42a11613e69e9c5ce484126f5c5be30cd14628df
root <root@95ad4591-2629-e011-b8a3-0018f302bd40>
2012-03-14 14:01:07 +0000

    Added ltrim (fix bug^ double slaches)
    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@708 95ad4591-2629-e011-b8a3-0018f302bd40

--- libs/smarty/plugins/function.gl.php
+++ libs/smarty/plugins/function.gl.php
@@ -3,7 +3,7 @@ function smarty_function_gl($params, $template) {
 	$p=$template->getTemplateVars('_gsparams');
 	$d=$template->getTemplateVars('_gsdata');
 	if (isset($params['module_name'])) $p['module_name']=$params['module_name'];
-    return cfg('www_root').'/'.call_user_func($p['module_name'].'::gl',key($params),current($params),$d['gspgid']);
+    return cfg('www_root').'/'.ltrim(call_user_func($p['module_name'].'::gl',key($params),current($params),$d['gspgid']),'/');
 }
 
 ?>
