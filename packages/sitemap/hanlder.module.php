deba45c9baa14d096ccc0baa187888f61e0193a5
alexk <alexk@95ad4591-2629-e011-b8a3-0018f302bd40>
2012-04-13 15:09:22 +0000

    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@743 95ad4591-2629-e011-b8a3-0018f302bd40

---
+++ packages/sitemap/hanlder.module.php
@@ -0,0 +1,39 @@
+<?php
+
+class sitemap_cfg_form extends g_forms_html {
+	function __construct($hh,$params=array(),$data=array()) {
+		//$c=str_replace('module_','',class_members('gs_module'));
+		$this->field_options['module_name']['variants']=class_members('gs_module');
+		$this->field_options['recordset_name']['variants']=class_members('gs_recordset_short');
+		parent::__construct($hh,$params,$data);
+	}
+}
+
+class sitemap_handler extends gs_base_handler {
+	function execute($ret) {
+		setlocale(LC_NUMERIC,'POSIX');
+
+		$xml=new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>');
+		$xml->lastmod=date('W3C');
+		$xml->priority=1.0;
+		$xml->changefreq='daily';
+
+		$rs=new sitemap_cfg();
+		$rs->find_records(array('disabled'=>0));
+		foreach ($rs as $rec) {
+			$xrs=new $rec->recordset_name;
+			foreach ($xrs->find_records(array()) as $xrec) {
+				$url=call_user_func($rec->module_name.'::gl',$rec->gl,$xrec,$this->data['gspgid'],'/');
+				$url='/'.ltrim($url,'/');
+
+				$i=$xml->addChild('url');
+				$i->loc=$url;
+				if($xrec->_ctime) $i->lastmod=date('W3C',strtotime($xrec->_ctime));
+				$i->changefreq='weekly';
+				$i->priority=0.5;
+
+			}
+		}
+		return $xml;
+	}
+}
