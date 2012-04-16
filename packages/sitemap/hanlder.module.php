e5fb75db6a46a693c114a81910d420f65d14c489
alexk <alexk@95ad4591-2629-e011-b8a3-0018f302bd40>
2012-04-16 16:25:07 +0000

    limit 1000 recs and full hostname in the sitemap
    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@748 95ad4591-2629-e011-b8a3-0018f302bd40

--- packages/sitemap/hanlder.module.php
+++ packages/sitemap/hanlder.module.php
@@ -11,6 +11,7 @@ class sitemap_cfg_form extends g_forms_html {
 
 class sitemap_handler extends gs_base_handler {
 	function execute($ret) {
+		$host='http://'.cfg('host');
 		setlocale(LC_NUMERIC,'POSIX');
 
 		$xml=new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>');
@@ -22,9 +23,9 @@ class sitemap_handler extends gs_base_handler {
 		$rs->find_records(array('disabled'=>0));
 		foreach ($rs as $rec) {
 			$xrs=new $rec->recordset_name;
-			foreach ($xrs->find_records(array()) as $xrec) {
+			foreach ($xrs->find_records(array())->limit(1000) as $xrec) {
 				$url=call_user_func($rec->module_name.'::gl',$rec->gl,$xrec,$this->data['gspgid'],'/');
-				$url='/'.ltrim($url,'/');
+				$url=$host.'/'.ltrim($url,'/');
 
 				$i=$xml->addChild('url');
 				$i->loc=$url;
