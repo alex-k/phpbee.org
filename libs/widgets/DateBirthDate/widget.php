52df162377d78c8d39f50296b6507904bf254afc
root <root@95ad4591-2629-e011-b8a3-0018f302bd40>
2012-11-30 13:32:14 +0000

    Fixed bugs into widget DateBirthDate
    git-svn-id: svn+ssh://phpbee.org/home/pahomov/www/phpbee.org/svn/phpbee.org@852 95ad4591-2629-e011-b8a3-0018f302bd40

--- libs/widgets/DateBirthDate/widget.php
+++ libs/widgets/DateBirthDate/widget.php
@@ -5,7 +5,15 @@ class gs_widget_DateBirthDate extends gs_widget{
 		$tpl=gs_tpl::get_instance();
 		$tpl->template_dir[]=dirname(__FILE__).DIRECTORY_SEPARATOR.'templates';
 		$params=$this->params;
-		$params['value']=$this->value;
+		
+		if (!is_array($this->value)) {
+			$date=strtotime($this->value);
+			$params['value']['Date_Year']=date('Y',$date);
+			$params['value']['Date_Month']=date('m',$date);
+			$params['value']['Date_Day']=date('d',$date);
+		} else {
+			$params['value']=$this->value;
+		}
 		$params['fieldname']=$this->fieldname;
 
 		$tpl->assign('params',$params);
